84ace122d84e7fe5a2a7c2e97c8f52217412e441
Abhijith Mundanad <mabhijithn@gmail.com>
2021-03-07 16:59:18 +0100

    Fixed setup; now eeganalyse can be imported

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='eeganalyse',
       description='A Simple, easily re-useable toolbox in python which will help researchers read, analyse and visualize EEG recorded and distributed in various formats.',
       author='Abhijith Mundanad Narayanan',
       author_email='mabhijithn@fastmail.com',
-      packages=['eegbasic'],
+      packages=['eeganalyse'],
       classifiers=[
           'Programming Language :: Python :: 3',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
